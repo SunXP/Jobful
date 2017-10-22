5238f3f8abab2a948cf6de9205b7a9da284a20d2
SunXP <roger_lu_dude@hotmail.com>
2017-10-22 03:01:25 -0400

    added request package for http req and res

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var express = require("express");
 var app = express();
+var request = require("request");
 
 app.set("view engine", "ejs");
+
+// request('http://www.google.com', function(error, response, body){
+//     if(error){
+//         console.log("something wrong");
+//         console.log(error);
+//     } else {
+//         if(response.statusCode==200){
+//             console.log(body)
+//         }
+//     }
+//     });
 
 app.get("/", function(req, res){
     res.render("landing")
